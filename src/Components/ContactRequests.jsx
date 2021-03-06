79c99a2c0059b2d84bcdc739f03826b2fa0649bc
ffzsina <f.zsina0@gmail.com>
2021-03-06 21:32:41 +0100

    fake pagination

--- src/Components/ContactRequests.jsx
+++ src/Components/ContactRequests.jsx
@@ -9,16 +9,24 @@ export function ContactList(props){
     const langCntx = React.useContext(LangContext);
 
     const [finder, setFinder] = useState("");
+    const [firstListItem, setFirstListItem] = useState(0);
 
     const handleChangeFinder = event => {
         setFinder(event.target.value);
+        setFirstListItem(0);
     }
 
-    function filterFunction(searched){
+    function filterBySearch(searched){
         if(langCntx.langGetSet[0]==="eng"){
             return (searched.name.firstName+" "+searched.name.lastName).toLowerCase().includes(finder.toLowerCase());
         }
         return (searched.name.lastName+" "+searched.name.firstName).toLowerCase().includes(finder.toLowerCase());
+    }
+
+    const searchedNames = props.contacts.filter(searched => filterBySearch(searched));
+
+    function filterByPagination(i, range){
+        return (i >= firstListItem && i < (firstListItem+range));
     }
 
     return (
@@ -26,8 +34,8 @@ export function ContactList(props){
         <div id="VerticalContactList" className="border shadow p-3 mb-4 mt-4 rounded">
             <div className="row">
                 <h3 className="col">{langCntx.dict[langCntx.langGetSet[0]].contacts}</h3>
-                <div className="col m-2">
-                    <FontAwesomeIcon icon={faSearch}/>
+                <div className="col m-2 input-group">
+                    <h5 className="p-1">{<FontAwesomeIcon icon={faSearch}/>}</h5>
                     <input type="text" className="form-control"
                         placeholder={langCntx.dict[langCntx.langGetSet[0]].typeAName}
                         onChange={handleChangeFinder}
@@ -35,12 +43,13 @@ export function ContactList(props){
                 </div>
             </div>
             <ul className="list-group" id="ContactList">
-                <li className="list-group-item bg-secondary text-white text-center mb-1"
-                    style={{cursor: "pointer"}}>
+                <li className={"list-group-item text-white text-center mb-1" + (firstListItem<=0 ? " disabled bg-light" : " bg-secondary")}
+                    style={{cursor: "pointer"}} onClick={() => setFirstListItem(prev => (prev - 1))}
+                >
                     <FontAwesomeIcon icon={faArrowUp}/>
                 </li>
-                {props.contacts
-                .filter(searched => filterFunction(searched))
+                {searchedNames
+                .filter((listItem, i) => filterByPagination(i,5))
                 .map(contact =>
                     <li key={contact.id} className={"list-group-item border" + (contact===props.selectedContact ? " bg-dark text-white" : "")}
                         style={{cursor: "pointer"}} onClick={() => {props.setSelectedContact(contact);}}
@@ -52,28 +61,34 @@ export function ContactList(props){
                         }
                     </li>    
                 )}
-                <li className="list-group-item bg-secondary text-white text-center mt-1"
-                    style={{cursor: "pointer"}}>
+                <li className={"list-group-item text-white text-center mb-1" + (firstListItem>=searchedNames.length-5 ? " disabled bg-light" : " bg-secondary")}
+                    style={{cursor: "pointer"}} onClick={() => setFirstListItem(prev => (prev + 1))}
+                >
                     <FontAwesomeIcon icon={faArrowDown}/>
                 </li>
             </ul>
         </div>
         <div id="HorizontalContactList" className="border shadow p-3 mb-4 mt-4 rounded">
-            <h3>{langCntx.dict[langCntx.langGetSet[0]].contacts}</h3>
-            <div className="col m-2">
-                <FontAwesomeIcon icon={faSearch}/>
-                <input type="text" className="form-control"
-                    placeholder={langCntx.dict[langCntx.langGetSet[0]].typeAName}
-                    onChange={handleChangeFinder}
-                />
+            <div className="row">
+                <h3 className="col">{langCntx.dict[langCntx.langGetSet[0]].contacts}</h3>
+                <div className="col m-2 input-group">
+                    <h5 className="p-1">{<FontAwesomeIcon icon={faSearch}/>}</h5>
+                    <input type="text" className="form-control"
+                        placeholder={langCntx.dict[langCntx.langGetSet[0]].typeAName}
+                        onChange={handleChangeFinder}
+                    />
+                </div>
             </div>
             <div className="row">
                 <Fragment>
-                    <button className="btn btn-sm btn-secondary mr-1 ml-3">
+                    <button className="btn btn-sm btn-secondary mr-1 ml-3"
+                        disabled={firstListItem<=0 ? true : false}
+                        onClick={() => setFirstListItem(prev => (prev - 1))}
+                    >
                         <FontAwesomeIcon icon={faArrowLeft}/>
                     </button>
-                    {props.contacts
-                    .filter(searched => filterFunction(searched))
+                    {searchedNames
+                    .filter((listItem, i) => filterByPagination(i,3))
                     .map(contact =>
                         <div key={contact.id} className={"col border rounded text-center" + (contact===props.selectedContact ? " bg-dark text-white" : "")}
                             style={{cursor: "pointer"}} onClick={() => {props.setSelectedContact(contact);}}
@@ -85,7 +100,10 @@ export function ContactList(props){
                         }
                         </div>    
                     )}
-                    <button className="btn btn-sm btn-secondary ml-1 mr-3">
+                    <button className="btn btn-sm btn-secondary ml-1 mr-3"
+                        disabled={firstListItem>=searchedNames.length-3 ? true : false}
+                        onClick={() => setFirstListItem(prev => (prev + 1))}
+                    >
                         <FontAwesomeIcon icon={faArrowRight}/>
                     </button>
                 </Fragment>
